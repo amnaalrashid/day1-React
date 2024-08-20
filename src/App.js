6bf495afd5876e6718168c79a8c9594aba170cf9
amnaalrashid <amna.alrashid@gmail.com>
2024-08-20 20:58:36 +0300

    Done Day1react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./assets/css/starter.css";
+import bakerystore from "./assets/media/bakery-store.png";
+import product from "./assets/data/products";
+
+const displayBakery = product.map((products) => {
+  return (
+    <div className="bakerystore-container">
+      <h2>{products.name}</h2>
+      <img className="bakery-items" src={products.image} alt="Bread"></img>
+      <p>{products.price}</p>
+    </div>
+  );
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Bakers World </h1>
+      <p1>Unleash Your Inner Bakers</p1>
+      <img src={bakerystore} alt="Bakery" className="bakerystore-image"></img>
+      <div className="bakery-section">{displayBakery}</div>
     </div>
   );
 }
---
+++ src/assets/data/products.js
@@ -0,0 +1,21 @@
+const product = [
+  {
+    name: "Bread",
+    price: "1.5 KD",
+    image:
+      "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcQwu9l5QEVRGPd9-hQ90jhpiGAMg1ycT46XWkXivXoCYaZ2RUm0Fz_Y0ZVxsJ50o3_K",
+  },
+  {
+    name: "Bagels",
+    price: "1 KD",
+    image:
+      "https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcQsnCE6lx3Nx1qREE8OvYdSMfcZBHru4mh-Kjass6_VnQJm0DR2VR4xM1TG8cTFoQem",
+  },
+  {
+    name: "Muffins",
+    price: ".750 KD",
+    image:
+      "https://sugargeekshow.com/wp-content/uploads/2019/10/chocolate-chip-muffins-featured.jpg",
+  },
+];
+export default product;
